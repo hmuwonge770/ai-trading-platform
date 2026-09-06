3de214fad0a0fb2b2da9de4fc5eaabc3fbb65323
Muwonge Hassan <hassansaava89@gmail.com>
2026-09-07 02:35:13 +0300

    feat(dashboard): establish dashboard auth boundary

---
+++ dashboard/config/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'defaults' => [
+        'guard' => 'web',
+        'passwords' => 'users',
+    ],
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+    ],
+    'providers' => [
+        'users' => [
+            'driver' => 'array',
+        ],
+    ],
+];

17f0b7bb6650aff9eab8477e4ecb423df9602198
Muwonge Hassan <hassansaava89@gmail.com>
2026-09-06 12:07:19 +0300

    feat: export backtesting engine

--- packages/backtesting/__init__.py
+++ packages/backtesting/__init__.py
@@ -1,1 +1,6 @@
 """Deterministic historical strategy simulation."""
+
+from packages.backtesting.engine import BacktestEngine
+from packages.backtesting.models import BacktestResult, BacktestTrade, EquityPoint
+
+__all__ = ["BacktestEngine", "BacktestResult", "BacktestTrade", "EquityPoint"]

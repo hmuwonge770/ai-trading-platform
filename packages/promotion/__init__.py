f8f299dbe6c8ca0c8823f0b2d4c7867b202bda1d
Muwonge Hassan <hassansaava89@gmail.com>
2026-09-06 22:46:24 +0300

    feat: export promotion PostgreSQL repository

--- packages/promotion/__init__.py
+++ packages/promotion/__init__.py
@@ -13,11 +13,12 @@ from .domain import (
 )
 from .invalidation import AuthorizationBinding, AuthorizationValidator
 from .preflight import LivePreflight, PreflightContext, PreflightResult
+from .repository import PromotionRepository
 from .service import PromotionService
 
 __all__ = [
     "ApprovalDecision", "ApprovalRole", "AuthorizationSnapshot", "CapitalAllocation",
-    "Promotion", "PromotionService", "PromotionStage", "PromotionStatus", "StrategyVersion",
+    "Promotion", "PromotionRepository", "PromotionService", "PromotionStage", "PromotionStatus", "StrategyVersion",
     "CanaryController", "CanaryLimits", "CanaryState", "AuthorizationBinding", "AuthorizationValidator",
     "LivePreflight", "PreflightContext", "PreflightResult",
 ]

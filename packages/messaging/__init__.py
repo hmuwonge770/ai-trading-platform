76bdb76e8479799429d99f49bbc88a72326d1b60
Muwonge Hassan <hassansaava89@gmail.com>
2026-09-06 16:33:36 +0300

    feat: expose Stage 20 outbox infrastructure

--- packages/messaging/__init__.py
+++ packages/messaging/__init__.py
@@ -1,6 +1,7 @@
 """RabbitMQ and asynchronous messaging infrastructure."""
 
 from packages.messaging.jobs import JobSpec
+from packages.messaging.outbox import OutboxDispatcher, OutboxRepository
 from packages.messaging.publisher import RabbitPublisher
 
-__all__ = ["JobSpec", "RabbitPublisher"]
+__all__ = ["JobSpec", "OutboxDispatcher", "OutboxRepository", "RabbitPublisher"]

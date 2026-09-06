69f25ea900292b326b6e2779b7be5f50109ec63f
Muwonge Hassan <hassansaava89@gmail.com>
2026-09-06 15:13:41 +0300

    feat: expose Stage 11 research controls

--- packages/experiments/__init__.py
+++ packages/experiments/__init__.py
@@ -1,6 +1,14 @@
 """Reproducible experiment management for quantitative research."""
 
+from packages.experiments.lineage import AntiOverfittingPolicy, LineageValidationError, dataset_fingerprint
 from packages.experiments.models import DatasetSplitConfig, ExperimentConfig
 from packages.experiments.runner import ExperimentRunner
 
-__all__ = ["DatasetSplitConfig", "ExperimentConfig", "ExperimentRunner"]
+__all__ = [
+    "AntiOverfittingPolicy",
+    "DatasetSplitConfig",
+    "ExperimentConfig",
+    "ExperimentRunner",
+    "LineageValidationError",
+    "dataset_fingerprint",
+]

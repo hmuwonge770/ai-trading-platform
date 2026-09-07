fe2f1a0adcd58360b70cc8cb7eed47c1de3c45d2
Muwonge Hassan <hassansaava89@gmail.com>
2026-09-07 05:54:34 +0300

    feat(autonomy): export continuous signal loop

--- packages/autonomy/__init__.py
+++ packages/autonomy/__init__.py
@@ -1,5 +1,12 @@
 """Autonomous trading control-plane primitives."""
 
 from .control import AutonomousControl, AutonomousMode, AutonomousState
+from .loop import AutonomousSignalLoop, MarketEvent
 
-__all__ = ["AutonomousControl", "AutonomousMode", "AutonomousState"]
+__all__ = [
+    "AutonomousControl",
+    "AutonomousMode",
+    "AutonomousState",
+    "AutonomousSignalLoop",
+    "MarketEvent",
+]

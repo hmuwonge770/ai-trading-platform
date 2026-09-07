a2941834a30c64888dea2a93e86b8235726768a1
Muwonge Hassan <hassansaava89@gmail.com>
2026-09-07 05:34:11 +0300

    feat: add autonomous control plane

---
+++ packages/autonomy/__init__.py
@@ -0,0 +1,5 @@
+"""Autonomous trading control-plane primitives."""
+
+from .control import AutonomousControl, AutonomousMode, AutonomousState
+
+__all__ = ["AutonomousControl", "AutonomousMode", "AutonomousState"]

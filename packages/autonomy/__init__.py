4ad52b3f53dc3513ea42190af585a7ba1c19d4e7
Muwonge Hassan <hassansaava89@gmail.com>
2026-09-07 06:33:04 +0300

    feat(autonomy): export market intelligence types

--- packages/autonomy/__init__.py
+++ packages/autonomy/__init__.py
@@ -1,6 +1,15 @@
 """Autonomous trading control-plane primitives."""
 
 from .control import AutonomousControl, AutonomousMode, AutonomousState
+from .intelligence import (
+    AISignalModel,
+    AISignalProposal,
+    AutonomousMarketIntelligence,
+    DeterministicRegimeDetector,
+    IntelligencePolicy,
+    MarketRegime,
+    RegimeAssessment,
+)
 from .loop import AutonomousSignalLoop, MarketEvent
 
 __all__ = [
@@ -9,4 +18,11 @@ __all__ = [
     "AutonomousState",
     "AutonomousSignalLoop",
     "MarketEvent",
+    "AISignalModel",
+    "AISignalProposal",
+    "AutonomousMarketIntelligence",
+    "DeterministicRegimeDetector",
+    "IntelligencePolicy",
+    "MarketRegime",
+    "RegimeAssessment",
 ]

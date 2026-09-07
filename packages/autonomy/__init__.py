90c1468c6dcb52e5328a700c0118772cce4cc762
Muwonge Hassan <hassansaava89@gmail.com>
2026-09-07 13:30:36 +0300

    feat(autonomy): export authorization preflight

--- packages/autonomy/__init__.py
+++ packages/autonomy/__init__.py
@@ -1,55 +1,24 @@
 """Autonomous trading control-plane primitives."""
 
+from .authorization_preflight import AuthorizationPreflightReport, AuthorizationPreflightStatus, AutonomousLiveAuthorizationPreflight
 from .control import AutonomousControl, AutonomousMode, AutonomousState
 from .execution import AutonomousExecutionLoop, AutonomousExecutionOutcome, ExecutionSubmitter
-from .intelligence import (
-    AISignalModel,
-    AISignalProposal,
-    AutonomousMarketIntelligence,
-    DeterministicRegimeDetector,
-    IntelligencePolicy,
-    MarketRegime,
-    RegimeAssessment,
-)
+from .intelligence import AISignalModel, AISignalProposal, AutonomousMarketIntelligence, DeterministicRegimeDetector, IntelligencePolicy, MarketRegime, RegimeAssessment
 from .learning import DriftAssessment, LearningAction, LearningPolicy, StrategyLearningEngine, StrategyPerformance
 from .loop import AutonomousSignalLoop, MarketEvent
 from .paper_runner import AutonomousPaperRunner, PaperRunOutcome
-from .performance import (
-    AutonomousTestnetPerformanceMonitor,
-    ExecutionObservation,
-    PerformancePolicy,
-    PerformanceReport,
-    PerformanceStatus,
-)
+from .performance import AutonomousTestnetPerformanceMonitor, ExecutionObservation, PerformancePolicy, PerformanceReport, PerformanceStatus
 from .positions import AutonomousPositionAgent, ManagedPosition, PositionAction, PositionDecision, PositionPolicy
-from .promotion import (
-    AutonomousPromotionReadinessGate,
-    PromotionEvidence,
-    PromotionReadinessPolicy,
-    PromotionReadinessReport,
-    PromotionReadinessStatus,
-)
-from .promotion_integration import (
-    AutonomousPromotionWorkflowIntegration,
-    PromotionEvidenceBinding,
-    PromotionHandoffStatus,
-)
+from .promotion import AutonomousPromotionReadinessGate, PromotionEvidence, PromotionReadinessPolicy, PromotionReadinessReport, PromotionReadinessStatus
+from .promotion_integration import AutonomousPromotionWorkflowIntegration, PromotionEvidenceBinding, PromotionHandoffStatus
 from .recovery import AutonomousRecoveryEngine, RecoveryAction, RecoveryDecision, RecoveryEvent, RecoveryPolicy, RecoveryState
-from .reconciliation import (
-    AutonomousTestnetReconciler,
-    ExchangeSnapshot,
-    ExpectedOrder,
-    ObservedOrder,
-    ReconciliationPolicy,
-    ReconciliationResult,
-    ReconciliationStatus,
-    TestnetStateProvider,
-)
+from .reconciliation import AutonomousTestnetReconciler, ExchangeSnapshot, ExpectedOrder, ObservedOrder, ReconciliationPolicy, ReconciliationResult, ReconciliationStatus, TestnetStateProvider
 from .risk import AutonomousRiskEngine, AutonomousRiskPolicy, AutonomousRiskResult
 from .testnet import BinanceTestnetExecutionSubmitter, TestnetExecutionPolicy, TestnetExecutionTransport
 from .testnet_runner import AutonomousTestnetRunner, TestnetRunOutcome
 
 __all__ = [
+    "AuthorizationPreflightReport", "AuthorizationPreflightStatus", "AutonomousLiveAuthorizationPreflight",
     "AutonomousControl", "AutonomousMode", "AutonomousState", "AutonomousSignalLoop", "MarketEvent",
     "AISignalModel", "AISignalProposal", "AutonomousMarketIntelligence", "DeterministicRegimeDetector",
     "IntelligencePolicy", "MarketRegime", "RegimeAssessment", "AutonomousRiskEngine", "AutonomousRiskPolicy",
